5ca121564afc9e997498b276eb0487d87531e0d7
Cristian Alvarado <ocdude@bluewavedigital.net>
2014-04-16 09:24:56 -0700

    rough working prototype. good enough in an emergency.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 moodle19-forum-extractor
 ========================
 
+## Description
 Extract forum content from a moodle 1.9.x backup file and present it in HTML format
+
+## Usage
+
+From a command line, pass a moodle 1.9 backup file with user data as a parameter. For example:
+
+    $ python3 extract.py moodle-backup.zip
+
+The output will print to the console. If you want to output to a file, pipe the output to an output file. For example:
+
+    $ python3 extract.py moodle-backup.zip > out.html
--- extract.py
+++ extract.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
 import zipfile
-import HTMLParser
+import html.parser
 import sys
+import os
 import xml.etree.ElementTree as et
+from datetime import date
+import sqlite3
 
 class MoodleBackup:
     backup = ''
@@ -12,6 +15,7 @@ class MoodleBackup:
     forums = []
     forum_discussions = []
     forum_posts = []
+    users = []
     course_name = ''
 
     def __init__(self,backup_file):
@@ -25,18 +29,52 @@ class MoodleBackup:
         self.moodle_backup = et.parse(self.backup.open('moodle.xml')).getroot()
         self.course_name = self.moodle_backup.find('./COURSE/HEADER/FULLNAME').text
 
-        #find all the forums
-        html = HTMLParser.HTMLParser()
+        #find all the forums and add them to forum_discussions
+        parsed = html.parser.HTMLParser()
+        forum_discussions = []
+
         for forum in self.moodle_backup.findall('./COURSE/MODULES/MOD'):
             if forum.find('./MODTYPE').text == 'forum':
                 self.forums.append({'name':forum.find('./NAME').text,
-                    'description':html.unescape(forum.find('./INTRO').text)})
-                #the logic in this section needs to be figured out a bit better
+                    'description':parsed.unescape(forum.find('./INTRO').text)})
+
                 for discussion in forum.findall('./DISCUSSIONS/DISCUSSION'):
-                    self.forum_discussions.append({'discussion_id':discussion.find('./ID').text,
-                        'name':discussion.find('./NAME')})
+                    for post in discussion.findall('./POSTS/POST'):
+                        forum_discussions.append([
+                            int(discussion.find('./ID').text),
+                            post.find('./SUBJECT').text,
+                            int(post.find('./PARENT').text),
+                            int(post.find('./USERID').text),
+                            post.find('./MODIFIED').text,
+                            parsed.unescape(post.find('./MESSAGE').text)
+                            ])
+
+        self.forum_discussions = forum_discussions
+
+        #find all the users and add them to users
+        for user in self.moodle_backup.findall('./COURSE/USERS/USER'):
+            self.users.append([int(user.find('./ID').text),user.find('./FIRSTNAME').text,user.find('./LASTNAME').text])
+
     def extract(self):
-        pass
+        c = sqlite3.connect(':memory:')
+        c.execute('''CREATE TABLE if not exists users
+            (id int, firstname text, lastname text)''')
+        c.execute('''CREATE TABLE if not exists posts
+            (id int, subject text, parent int, userid int, modified int, message text)''')
+        c.executemany('INSERT INTO users VALUES(?,?,?)',self.users)
+        c.executemany('INSERT INTO posts VALUES(?,?,?,?,?,?)', self.forum_discussions)
+        c.commit()
+        print("<!doctype html>\n<html>\n<head>\n<title>"+self.course_name+"</title>\n</head>\n<body>")
+        print("<h1>Forum posts in: "+self.course_name+"</h1>")
+        for row in c.execute('SELECT subject,modified,message,firstname,lastname FROM posts as P JOIN users as u on P.userid=U.id'):
+            modified_date = date.fromtimestamp(row[1])
+            print("<h2>"+row[0]+"</h2>")
+            print("<h3>By: "+row[3]+" "+row[4]+"</h3>")
+            print("<h4>Written on: "+modified_date.strftime("%B %d %Y")+"</h4>")
+            print("<div>"+row[2]+"</div>")
+            
+        print("</body>\n</html>")
+
     def list(self):
         pass
 
@@ -46,6 +84,4 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     mb = MoodleBackup(args.backup_file)
-    mb.list()
-    print(mb.course_name)
-    print(mb.forums)
+    mb.extract()
